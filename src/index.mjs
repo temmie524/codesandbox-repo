b9b6cbdada663a56d3d2c783925849b75907c6b4
Toshi <57585714+temmie524@users.noreply.github.com>
2023-08-16 15:49:21 +0000

    Update index.html and index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -1,1 +1,42 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //div
+  const div = document.createElement("div");
+  div.className = "list-low";
+  //li
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  //buttn (完了)
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    //const compTaget = completeButton.parentNode;
+
+    
+ 
+  });
+
+  //buttn (削除)
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    //delete div tag
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("incomp-list").removeChild(deleteTarget);
+  });
+
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  document.getElementById("incomp-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
